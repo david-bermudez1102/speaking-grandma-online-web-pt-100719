94fe299a19f9ed5085b0336045387be8521bf2e1
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-08 02:13:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if(phrase==phrase.upcase)
+  if(phrase.upcase==phrase)
     "HUH?! SPEAK UP, SONNY!"
   elsif(phrase=="I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
