b3654126ee02acd6466d2e7e40f0beeecbfea319
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-08 02:12:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
-
 def speak_to_grandma(phrase)
   if(phrase==phrase.upcase)
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif(phrase=="I LOVE YOU GRANDMA!")
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
 end
